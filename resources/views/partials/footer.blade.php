6d7193092a8bb54d09d8f2a75f1e40c7ca84cd20
Tommaso Venza <tommasovenza@gmail.com>
2020-07-29 20:35:50 +0200

    aggiunto footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,25 @@
 <footer>
-    ciao io sono il footer
+   
+    <div class="upper-footer">
+        <div class="logo">
+            <img src="{{ asset('img/logo-white.png') }}" alt="logo-bianco">
+
+            <span>Sede operativa</span>
+            <span>Via Carducci 12 - 20123 Milano</span>
+            <span>Tel: 02-40031288</span>
+
+        </div>
+
+        <div class="info">
+            <ul>
+                <li><a href="#">Lezione gratuita</a></li>
+                <li><a href="#">Domande Frequenti</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Lavora con noi</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+
 </footer>
