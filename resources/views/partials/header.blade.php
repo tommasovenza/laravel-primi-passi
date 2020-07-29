a6ad31cf0c79bcada2aa643e70925e015a265d75
Tommaso Venza <tommasovenza@gmail.com>
2020-07-29 19:48:18 +0200

    inserita navbar responsive

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('img/logo.png') }}" alt="">
     </div>
 
-    <div class="container-nav">
+    <div class="nav-container clearfix">
         <nav>
             <ul>
                 <li><a href="{{ route('home') }}">Home</a></li>
@@ -11,8 +11,12 @@
                 <li><a href="#">Dopo il corso</a></li>
                 <li><a href="#">Lezione gratuita</a></li>
                 <li><a href="#">Assumi i nostri studenti</a></li>
-                <li><a href="#">Candidati ora</a></li>
+                <li><a href="#" class="white">Candidati ora</a></li>
             </ul>
         </nav>
+    </div>
+
+    <div class="hamburger-menu active">
+        <i class="fas fa-bars"></i>
     </div>
 </header>
